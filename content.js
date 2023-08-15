c0dd2c005d86b0e6dea07e42a8a970ec2f4e5352
Jason Lin <jason.lin@appier.com>
2023-08-15 14:49:00 +0800

    complete version

--- content.js
+++ content.js
@@ -1,15 +1,18 @@
-// Capture HTML of current page
-let blob = new Blob([document.documentElement.outerHTML], { type: 'text/html' });
+let serialized = new XMLSerializer().serializeToString(document);
+
+let blob = new Blob([serialized], { type: 'text/html' });
 let url = URL.createObjectURL(blob);
 
 chrome.runtime.sendMessage({
     action: 'downloadHTML',
     url: url,
     suggestedName: 'page.html'
-}, function(response) {
+}, function(response) 
+{
     if (chrome.runtime.lastError) {
         console.error("Error sending downloadHTML message:", chrome.runtime.lastError);
-    } else {
+    } 
+    else{
         console.log("HTML download message sent successfully");
     }
 });
